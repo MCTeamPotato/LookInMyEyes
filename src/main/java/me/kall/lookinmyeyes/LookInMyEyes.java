4bd675caf3037e172b2ced66ce49df6924b26abd
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-18 20:33:47 +0800

    neoforge port

--- src/main/java/me/kall/lookinmyeyes/LookInMyEyes.java
+++ src/main/java/me/kall/lookinmyeyes/LookInMyEyes.java
@@ -3,13 +3,15 @@ package me.kall.lookinmyeyes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingChangeTargetEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingChangeTargetEvent;
 import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,23 +19,23 @@ import org.jetbrains.annotations.NotNull;
 public final class LookInMyEyes {
     public static final String MOD_ID = "lookinmyeyes";
 
-    public static final ForgeConfigSpec CONFIG;
-    public static final ForgeConfigSpec.DoubleValue VIEW_FIELD;
+    public static final ModConfigSpec CONFIG;
+    public static final ModConfigSpec.DoubleValue VIEW_FIELD;
 
     static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
         builder.push("LookInMyEyes");
         VIEW_FIELD = builder.defineInRange("ViewField(Angle, default 180°)", 180.00, 0.00, 360.00);
         builder.pop();
         CONFIG = builder.build();
     }
 
-    public LookInMyEyes(@NotNull FMLJavaModLoadingContext context) {
+    public LookInMyEyes(IEventBus modEventBus, Dist dist, @NotNull ModContainer container) {
         LogManager.getLogger(LookInMyEyes.class).info("Look in my eyes!");
-        context.registerConfig(ModConfig.Type.COMMON, CONFIG);
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, (LivingChangeTargetEvent event) -> {
+        container.registerConfig(ModConfig.Type.COMMON, CONFIG);
+        NeoForge.EVENT_BUS.addListener(EventPriority.LOWEST, (LivingChangeTargetEvent event) -> {
             if (event.isCanceled()) return;
-            LivingEntity target = event.getNewTarget();
+            LivingEntity target = event.getNewAboutToBeSetTarget();
             LivingEntity observer = event.getEntity();
             Level level = observer.level();
             if (level.isClientSide() || target == null) return;
