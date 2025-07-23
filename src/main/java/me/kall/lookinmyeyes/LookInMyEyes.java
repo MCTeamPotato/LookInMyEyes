7825554de029f695b71d9310865269d219782f45
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-23 14:28:56 +0800

    Update LookInMyEyes.java

--- src/main/java/me/kall/lookinmyeyes/LookInMyEyes.java
+++ src/main/java/me/kall/lookinmyeyes/LookInMyEyes.java
@@ -82,7 +82,7 @@ public final class LookInMyEyes {
         if (event.isCanceled()) return;
         LivingEntity target = event.getNewTarget();
         LivingEntity observer = event.getEntity();
-        if (observer.level().isClientSide() || target == null) return;
+        if (observer.level.isClientSide() || target == null) return;
         if (observer.getPersistentData().getBoolean(MOD_ID)) {
             observer.getPersistentData().remove(MOD_ID);
              return;
